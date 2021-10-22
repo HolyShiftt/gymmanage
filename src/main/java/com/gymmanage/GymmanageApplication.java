8438f08b2c73796a20ba645b955032477d88ccca
HolyShiftt <1148088067@qq.com>
2021-10-22 17:14:19 +0800

    基础建设

---
+++ src/main/java/com/gymmanage/GymmanageApplication.java
@@ -0,0 +1,13 @@
+package com.gymmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymmanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gymmanage/gymmanage/GymmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymmanage.gymmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
